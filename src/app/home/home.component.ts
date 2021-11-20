9752a80484a70ef124b939e41f74a77c117b8790
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-11-20 11:00:24 +0000

    Add redirections by using ?page= for index.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.page === 'privacy' || params.page === 'privacy-policy') {
+        this.router.navigate(['/privacy-policy']);
+      }
+    });
+  }
 }
