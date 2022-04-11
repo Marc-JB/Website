a20c6b20afc7677c3efa5360cf958e87cff25936
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-04-11 17:17:30 +0200

    Use modern CSS (grid) and SCSS capabilities

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { MatToolbar } from '@angular/material/toolbar';
 
 export enum HeaderStyle {
     primary, primaryElevated, transparent, transparentDark
@@ -14,15 +16,36 @@ export class HeaderComponent implements OnInit {
     @Input() menu: any = null;
     @Input() maxWidth = 'auto';
 
-    constructor() { }
+    @ViewChild(MatToolbar)
+    toolbarElement: MatToolbar | undefined;
 
     get isTransparent(): boolean {
         return this.headerStyle === HeaderStyle.transparent || this.headerStyle === HeaderStyle.transparentDark;
+    }
+
+    get hasElevation(): boolean {
+        return this.headerStyle === HeaderStyle.primaryElevated
+    }
+
+    get isDarkThemed(): boolean {
+        return this.headerStyle === HeaderStyle.transparentDark
     }
 
     get maximumWidth(): string {
         return `max-width: ${this.maxWidth};`;
     }
 
+    get toolbarColor(): ThemePalette {
+        return this.isTransparent ? undefined : "primary"
+    }
+
+    constructor() { }
+
     ngOnInit(): void { }
+
+    ngAfterViewInit(): void {
+        if (this.toolbarElement !== undefined) {
+            this.toolbarElement.color = this.toolbarColor
+        }
+    }
 }
