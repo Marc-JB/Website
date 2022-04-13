33c577b87e72033b65644026164f481fb81437fa
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-04-13 11:11:21 +0200

    Create app menu component & make tests pass

--- src/test/privacy-policy/privacy-policy.component.spec.ts
+++ src/test/privacy-policy/privacy-policy.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { PrivacyPolicyComponent } from '../../app/privacy-policy/privacy-policy.component';
 
 describe('PrivacyPolicyComponent', () => {
+    let component: PrivacyPolicyComponent;
+    let fixture: ComponentFixture<PrivacyPolicyComponent>;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [
@@ -10,13 +13,18 @@ describe('PrivacyPolicyComponent', () => {
             ],
             declarations: [
                 PrivacyPolicyComponent
-            ],
-        }).compileComponents();
+            ]
+        })
+            .compileComponents();
     });
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(PrivacyPolicyComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PrivacyPolicyComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
     });
 });
