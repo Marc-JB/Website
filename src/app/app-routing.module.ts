6e70d9671915068b460afe548a0b9aec0ad0fe0c
Marc <16156117+Marc-JB@users.noreply.github.com>
2024-12-13 11:17:13 +0000

    Add contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LicensesComponent } from './licenses/licenses.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'privacy', component: PrivacyPolicyComponent },
     { path: 'licenses', component: LicensesComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'support', component: ContactComponent },
+    { path: 'help', component: ContactComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
 
